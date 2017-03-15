dfd957e85774255ce4d17e15559816f8ae749340
Oleksandra Holovina <Oleksandra_Holovina@epam.com>
2017-03-15 14:06:06 +0200

    Modify Game class -refactor -change sum method to use stream

--- src/main/java/com/holovina/Game.java
+++ src/main/java/com/holovina/Game.java
@@ -3,72 +3,62 @@ package com.holovina;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Oleksandra_Holovina on 3/15/2017.
- */
 public class Game {
-    private List<Integer> left = new ArrayList<Integer>();
-    private List<Integer> right = new ArrayList<Integer>();
-
+    private List<Integer> firTeamKicks = new ArrayList<>();
+    private List<Integer> secTeamKicks = new ArrayList<>();
 
     private int sum(List<Integer> lst) {
-        int score = 0;
-        for (Integer l : lst) {
-            score += l;
-        }
-        return score;
+       return lst.stream().mapToInt(i -> i).sum();
     }
 
     public boolean finished() {
-        if (left.size() < 5 || right.size() < 5) {
+        if (firTeamKicks.size() < 5 || secTeamKicks.size() < 5) {
             return false;
         }
 
-        if (Math.abs(left.size()-right.size()) == 1){
+        if (Math.abs(firTeamKicks.size()- secTeamKicks.size()) == 1){
             return false;
         }
 
-        if (sum(left) != sum(right)) {
+        if (sum(firTeamKicks) != sum(secTeamKicks)) {
             return true;
         }
 
         return false;
     }
 
-    public Integer [] kick(String playerName, int team, int kick) throws IllegalKickExecption {
+    public Integer [] kick(String playerName, int team, int kick) throws IllegalKickException {
         if (finished()){
-            throw new IllegalKickExecption();
+            throw new IllegalKickException();
         }
         if (team == 0) {
-            left.add(kick);
+            firTeamKicks.add(kick);
         } else {
-            right.add(kick);
+            secTeamKicks.add(kick);
         }
 
         return loadLast10(playerName);
     }
 
-
     public int winner() {
         if (finished()) {
-            int leftScore = sum(left);
-            int rightScore = sum(right);
+            int leftScore = sum(firTeamKicks);
+            int rightScore = sum(secTeamKicks);
             return leftScore > rightScore ? 0 : 1;
         }
         return -1;
     }
 
-
     public String score() {
-        return sum(left)+"["+ totalPlayerPriceNotKicked(0, left.size())+"]" + ":"
-                + sum(right)+"["+ totalPlayerPriceNotKicked(1, right.size())+"]";
+        return sum(firTeamKicks)+"["+ totalPlayerPrice(0, firTeamKicks.size())+"]" + ":"
+                + sum(secTeamKicks)+"["+ totalPlayerPrice(1, secTeamKicks.size())+"]";
     }
 
     public Integer [] loadLast10(String player) {
         return null;
     }
 
-    public double totalPlayerPriceNotKicked(int team, int serie){
+    public double totalPlayerPrice(int team, int serie){
         return 0;
     }
 }
