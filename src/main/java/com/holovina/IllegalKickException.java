08429ec14e18e3d4c70b3fc4ea7df38e7759b698
Oleksandra Holovina <Oleksandra_Holovina@epam.com>
2017-03-15 13:50:34 +0200

    Change maven version

--- src/main/java/com/holovina/IllegalKickException.java
+++ src/main/java/com/holovina/IllegalKickException.java
@@ -1,11 +1,8 @@
 package com.holovina;
 
-/**
- * Created by Oleksandra_Holovina on 3/15/2017.
- */
 public class IllegalKickExecption extends Exception{
     @Override
     public String toString() {
-        return "IllegalKickExecption{}";
+        return "The game is already finished. No more kicks allowed!";
     }
 }
